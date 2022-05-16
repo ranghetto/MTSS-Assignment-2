adcdd99497193bdf1d2357d15ec65bd577661f74
FakeDaemon <oselieroantonio@gmail.com>
2022-05-16 17:25:49 +0200

    implementazione metodo getNumberofCPU

--- src/main/java/it/unipd/mtss/business/Order.java
+++ src/main/java/it/unipd/mtss/business/Order.java
@@ -7,6 +7,7 @@ package it.unipd.mtss.business;
 import it.unipd.mtss.model.EItem;
 import it.unipd.mtss.model.OrderInterface;
 import it.unipd.mtss.model.User;
+import it.unipd.mtss.model.EItem.ItemType;
 
 import java.util.List;
 
@@ -15,5 +16,21 @@ public class Order implements OrderInterface {
     @Override
     public double getOrderPrice(List<EItem> itemsOrdered, User user) {
         return 0;
+    }
+
+    public int getNumberofCPU(List<EItem> itemsOrdered){
+        if(itemsOrdered.isEmpty())
+        {
+            return 0;
+        }
+        else
+        {
+            int n=0;
+            for(int i=0; i<itemsOrdered.size(); i++)
+            {
+                if(itemsOrdered.get(i).getType()==ItemType.PROCESSOR) ++n;
+            }
+            return n;
+        }
     }
 }
