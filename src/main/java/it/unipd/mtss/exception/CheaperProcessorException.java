eab3a82defb3d0c7bc27cd49cc39df90879d2591
FakeDaemon <oselieroantonio@gmail.com>
2022-05-16 23:13:20 +0200

    implementato sconto 50perc se comprati 5 proc
    close #5

---
+++ src/main/java/it/unipd/mtss/exception/CheaperProcessorException.java
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////
+// Matteo Rango 2008066
+// Antonio Oseliero 1226325
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss.exception;
+
+public class CheaperProcessorException extends Exception{
+    public CheaperProcessorException(String message){
+        super(message);
+    }
+}
--- src/test/java/it/unipd/mtss/business/OrderTest.java
+++ src/test/java/it/unipd/mtss/business/OrderTest.java
@@ -1,11 +1,13 @@
 package it.unipd.mtss.business;
 
+import it.unipd.mtss.exception.CheaperProcessorException;
 import it.unipd.mtss.model.EItem;
 import it.unipd.mtss.model.EItem.ItemType;
 import it.unipd.mtss.model.User;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,15 +27,30 @@ public class OrderTest {
     //getOrderPrice
 
     @Test
-    public void MethodGetOrderPriceShouldReturnTotal() {
+    public void MethodGetOrderPriceShouldReturnTotal() throws CheaperProcessorException {
         User user = new User(1, 22, "Aldo", "Giovanni");
-        List<EItem> list= Arrays.asList(
-                new EItem(ItemType.MOUSE, "a", 16.0),
-                new EItem(ItemType.KEYBOARD, "b", 30.0),
-                new EItem(ItemType.MOTHERBOARD, "c", 14.0)
+        list= Arrays.asList(
+            new EItem(ItemType.MOUSE, "a", 16.0),
+            new EItem(ItemType.KEYBOARD, "b", 30.0),
+            new EItem(ItemType.MOTHERBOARD, "c", 14.0)
         );
         assertEquals(60.0, order.getOrderPrice(list, user), 0.001);
         order.getOrderPrice(list, user);
+    }
+
+    @Test
+    public void MethodGetOrderPriceShouldDiscountCheaperProcessor() throws CheaperProcessorException{
+        User user = new User(1, 22, "Aldo", "Giovanni");
+        list= Arrays.asList(
+            new EItem(ItemType.PROCESSOR, "a", 30.0),
+            new EItem(ItemType.PROCESSOR, "b", 20.0),
+            new EItem(ItemType.PROCESSOR, "c", 10.0),
+            new EItem(ItemType.PROCESSOR, "d", 6.0),
+            new EItem(ItemType.PROCESSOR, "e", 5.0),
+            new EItem(ItemType.PROCESSOR, "f", 4.0),
+            new EItem(ItemType.MOUSE, "g", 5.0)
+        );
+        assertEquals(78, order.getOrderPrice(list, user), 0.001);
     }
 
     //getNumberOfCPU
@@ -45,32 +62,32 @@ public class OrderTest {
 
     @Test
     public void MethodGetNumberOfCPUShouldReturnCorrectValues(){
-        List<EItem> list= Arrays.asList(
+        list= Arrays.asList(
             new EItem(ItemType.PROCESSOR, "a", 12.0),
             new EItem(ItemType.PROCESSOR, "b", 30.0), 
             new EItem(ItemType.MOTHERBOARD, "c", 12.0)
         );
-        assertEquals(order.getNumberofCPU(list), 2);
+        assertEquals(2,order.getNumberofCPU(list));
     } 
 
     @Test
     public void MethodGetNumberOfCPUShouldReturn0IfNoProcessorInList(){
-        List<EItem> list= Arrays.asList(
+        list= Arrays.asList(
             new EItem(ItemType.MOUSE, "a", 12.0),
             new EItem(ItemType.KEYBOARD, "b", 30.0), 
             new EItem(ItemType.MOTHERBOARD, "c", 12.0)
         );
-        assertEquals(order.getNumberofCPU(list), 0);
+        assertEquals(0,order.getNumberofCPU(list));
     }
 
     // totalPrice
 
     @Test
     public void MethodTotalPriceShouldReturnCorrectValue(){
-        List<EItem> list= Arrays.asList(
-                new EItem(ItemType.MOUSE, "a", 12.0),
-                new EItem(ItemType.KEYBOARD, "b", 30.0),
-                new EItem(ItemType.MOTHERBOARD, "c", 12.0)
+        list= Arrays.asList(
+            new EItem(ItemType.MOUSE, "a", 12.0),
+            new EItem(ItemType.KEYBOARD, "b", 30.0),
+            new EItem(ItemType.MOTHERBOARD, "c", 12.0)
         );
         assertEquals(54.0, order.totalPrice(list), 0.001);
     }
@@ -79,4 +96,63 @@ public class OrderTest {
     public void MethodTotalPriceShouldReturn0IfListEmpty(){
         assertEquals(0.0, order.totalPrice(list), 0.001);
     }
+
+    // CheaperProcessor
+
+    @Test
+    public void MethodCheaperProcessorShouldReturnCheaperProcessorPrice() throws CheaperProcessorException {
+        list= Arrays.asList(
+            new EItem(ItemType.PROCESSOR, "a", 30.0),
+            new EItem(ItemType.PROCESSOR, "b", 20.0),
+            new EItem(ItemType.PROCESSOR, "c", 10.0),
+            new EItem(ItemType.PROCESSOR, "d", 6.0),
+            new EItem(ItemType.PROCESSOR, "e", 5.0),
+            new EItem(ItemType.PROCESSOR, "f", 4.0),
+            new EItem(ItemType.MOUSE, "g", 5.0)
+        );
+        assertEquals(4.0, order.FindCheaperProcessor(list),0.001);
+    }
+
+    @Test
+    public void MethodCheaperProcessorShouldLaunchExceptionOnFreeProcessor() throws CheaperProcessorException {
+        list= Arrays.asList(
+            new EItem(ItemType.PROCESSOR, "a", 30.0),
+            new EItem(ItemType.PROCESSOR, "b", 20.0),
+            new EItem(ItemType.PROCESSOR, "c", 10.0),
+            new EItem(ItemType.PROCESSOR, "d", 6.0),
+            new EItem(ItemType.PROCESSOR, "e", 5.0),
+            new EItem(ItemType.PROCESSOR, "f", 0.0),
+            new EItem(ItemType.MOUSE, "g", 5.0)
+        );
+        assertEquals(0, order.FindCheaperProcessor(list), 0.001); 
+    }
+
+    @Test(expected = CheaperProcessorException.class)
+    public void MethodCheaperProcessorShouldLaunchExceptionOnNoProcessor()
+            throws CheaperProcessorException {
+        list= Arrays.asList(
+            new EItem(ItemType.KEYBOARD, "a", 30.0),
+            new EItem(ItemType.MOUSE, "g", 5.0)
+        );
+        order.FindCheaperProcessor(list);
+    }
+
+    @Test(expected = CheaperProcessorException.class)
+    public void MethodCheaperProcessorShouldLaunchExceptionOnEmptyList()
+            throws CheaperProcessorException {
+        order.FindCheaperProcessor(list);
+    }
+
+    //Apply50PercDiscount
+
+    @Test
+    public void MethodApply50PercDiscountShouldReturn0OnValue0(){
+        assertEquals(0.0, order.Apply50PercDiscount(0.0), 0.001);
+    }
+
+    @Test
+    public void MetohdApply50PercDiscountShouldReturnCorrectValues(){
+        assertEquals(8, order.Apply50PercDiscount(16), 0.001);
+    }
+
 }
